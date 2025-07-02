809d36d90d54ca53e726fb5decb1cfee0f6d4f1e
MaceyN08 <macey.nowell@mscoding.org>
2025-07-02 12:45:14 -0500

    weeeeee

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const findMe = () => {
+  
+
+  const success = (position) => {
+    console.log(position);
+    status.textContent = "success";
+    const { latitude, longitude } = position.coords;
+    ;
+  };
+  const error = () => {
+    
+  };
+
+  navigator.geolocation.getCurrentPosition(success, error);
+};
